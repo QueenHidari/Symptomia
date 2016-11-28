6e6f1f8568229975b96fcbb6da30d6edc08dcd98
Hidari <toastergod@outlook.com>
2016-11-28 15:08:21 -0500

    Fixed Required Android SDK

--- Symptomia/ios/SymptomiaTests/SymptomiaTests.m
+++ Symptomia/ios/SymptomiaTests/SymptomiaTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface SymptomiaTests : XCTestCase
+@interface symptomiaTests : XCTestCase
 
 @end
 
-@implementation SymptomiaTests
+@implementation symptomiaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
