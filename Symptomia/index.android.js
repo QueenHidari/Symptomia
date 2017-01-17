62620c42ab81295b83cbb37daff9329836e0dbd2
Hidari <toastergod@outlook.com>
2017-01-17 15:17:57 -0500

    Updated and added drawer

--- Symptomia/index.android.js
+++ Symptomia/index.android.js
@@ -4,54 +4,76 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ReactNative, {Button, DrawerLayoutAndroid} from 'react-native'
 import Drawer from 'react-native-drawer'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 export default class symptomia extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Drawer
-            type="overlay"
-            content={
-              <View style={styles.main}></View>
-            }
-            tapToClose={true}
-            openDrawerOffset={0.2}
-            panCloseMask={0.2}
-            closedDrawerOffset={-3}
-            tweenHandler={(ratio) => ({
-              main: { opacity:(2-ratio)/2 }
-            })}
-          >
-        </Drawer>
 
+    var navigationView = (
+      <View style={{flex: 1, backgroundColor: '#556270'}}>
+        <View style={styles.navTitle}>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'center', color: '#FF6B6B'}}>Symptomia</Text>
+        </View>
+        <View style={styles.navButtons}>
+          <Button
+            onPress={null}
+            color="#FF6B6B"
+            accessabilityLabel="Calendar"
+            title="Calendar"
+          />
+          <Button
+            onPress={null}
+            color="#C44D58"
+            accessabilityLabel="Settings"
+            title="Settings"
+          />
+        </View>
       </View>
+    );
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerBackgroundColor="#272822"
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}
+        >
+          <View style={{flex: 1, alignItems: 'center'}}>
+            <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello world!</Text>
+          </View>
+      </DrawerLayoutAndroid>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
+  navTitle: {
+    backgroundColor: '#272822'
+  },
+  navButtons: {
+    flex: 1
+  },
   drawer: {
     shadowColor: '#000000',
     shadowOpacity: 0.8,
     shadowRadius: 3,
     backgroundColor: '#000000'
   },
+  text: {
+    color: '#FFFFFF',
+    margin: 10,
+    fontSize: 15
+  },
   main: {
     paddingLeft: 3
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2F2F2F',
   },
   welcome: {
     fontSize: 20,
